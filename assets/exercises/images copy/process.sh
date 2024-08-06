651ffa4e415357696f4aab050506f14e08a19dae
hh <hh@hhs-iMac-Pro.local>
2024-08-06 19:42:07 +0700

    removed spare images

--- assets/exercises/images copy/process.sh
+++
@@ -1,8 +0,0 @@
-find "./" -name '*.png' -exec sh -c '
-  for FILE; do
-    BASENAME=$(basename "$FILE")
-    DEST="../images2/$BASENAME"
-    convert "$FILE" -fuzz 5% -fill magenta -draw "color 0,0 floodfill" "$DEST"
-  done
-' sh {} +
-
